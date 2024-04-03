f4fc3562c1c643997c6d7705752bb204701c05d5
MansurSarkhanov <sarxanov.mansur@gmail.com>
2024-04-03 15:39:06 +0400

    test

--- burda_ui/lib/Presentation/Components/Sheets/user_sheets.dart
+++ burda_ui/lib/Presentation/Components/Sheets/user_sheets.dart
@@ -1,3 +1,4 @@
+import 'package:burda_ui/Presentation/Animation/bounce_animation.dart';
 import 'package:burda_ui/Presentation/Components/selection_menu.dart';
 import 'package:burda_ui/Provider/home_provider.dart';
 import 'package:flutter/material.dart';
@@ -18,119 +19,142 @@ void showSheet(context, HomeProvider provider, int index) {
         ),
         child: Padding(
           padding: const EdgeInsets.only(top: 8.0, left: 16, right: 16, bottom: 16),
-          child: Column(
-            children: [
-              Container(
-                width: 30,
-                height: 4,
-                decoration: BoxDecoration(color: Colors.grey.shade400, borderRadius: BorderRadius.circular(8)),
-              ),
-              sizedBoxH(8),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  const Text(
-                    "Profile",
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
-                  ),
-                  InkWell(
-                      onTap: () {
-                        Navigator.pop(context);
-                      },
-                      child: Container(
-                          decoration: const BoxDecoration(shape: BoxShape.circle, color: Color(0xFFF7F7F7)),
-                          child: const Padding(
-                            padding: EdgeInsets.all(8.0),
-                            child: Icon(Icons.close),
-                          )))
-                ],
-              ),
-              sizedBoxH(16),
-              Expanded(
-                child: Container(
-                  decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      image: DecorationImage(fit: BoxFit.contain, image: NetworkImage(provider.users[index].imageUrl))),
+          child: BounceFromBottomAnimation(
+            delay: 2,
+            child: Column(
+              children: [
+                Container(
+                  width: 30,
+                  height: 4,
+                  decoration: BoxDecoration(color: Colors.grey.shade400, borderRadius: BorderRadius.circular(8)),
                 ),
-              ),
-              sizedBoxH(8),
-              Text(
-                provider.users[index].name,
-                style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 22),
-              ),
-              sizedBoxH(4),
-              Text(
-                provider.users[index].pleace,
-                style: TextStyle(fontSize: 18, color: Colors.grey.shade400),
-              ),
-              Container(
-                margin: const EdgeInsets.symmetric(vertical: 24),
-                decoration: BoxDecoration(borderRadius: BorderRadius.circular(12), color: const Color(0xFFFDEAE8)),
-                child: Padding(
-                  padding: const EdgeInsets.all(6.0),
-                  child: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      const Icon(
-                        Icons.favorite,
-                        color: Colors.red,
-                      ),
-                      sizedBoxW(4),
-                      Text(provider.users[index].favorites.toString())
-                    ],
-                  ),
-                ),
-              ),
-              const Divider(),
-              sizedBoxH(12),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "Bio",
-                    style: TextStyle(color: Colors.grey.shade500),
-                  ),
-                  sizedBoxH(8),
-                  const Text(
-                    "Cat-lover, Designer, Weekend fan and biologist",
-                    style: TextStyle(fontSize: 18, color: Color(0xFF5E5E5E)),
-                  ),
-                  sizedBoxH(24),
-                  Row(
-                    children: [
-                      Expanded(
-                        child: ElevatedButton(
-                            onPressed: () {},
-                            style: ElevatedButton.styleFrom(
-                                backgroundColor: AppColors.orangeColor,
-                                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12))),
-                            child: const Center(
-                                child: Padding(
-                              padding: EdgeInsets.symmetric(vertical: 16.0),
-                              child: Text(
-                                "Send Message",
-                                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-                              ),
-                            ))),
-                      ),
-                      sizedBoxW(12),
-                      GestureDetector(
-                        onTapDown: (detail) {
-                          selectOperationMenu(context, detail);
+                sizedBoxH(8),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text(
+                      "Profile",
+                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+                    ),
+                    InkWell(
+                        onTap: () {
+                          Navigator.pop(context);
                         },
                         child: Container(
-                          decoration: BoxDecoration(shape: BoxShape.circle, border: Border.all(color: Colors.grey)),
-                          child: const Padding(
-                            padding: EdgeInsets.all(14.0),
-                            child: Icon(Icons.more_horiz_outlined),
+                            decoration: const BoxDecoration(shape: BoxShape.circle, color: Color(0xFFF7F7F7)),
+                            child: const Padding(
+                              padding: EdgeInsets.all(8.0),
+                              child: Icon(Icons.close),
+                            )))
+                  ],
+                ),
+                sizedBoxH(16),
+                Expanded(
+                  child: BounceFromBottomAnimation(
+                    delay: 3,
+                    child: Container(
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          image: DecorationImage(
+                              fit: BoxFit.contain, image: NetworkImage(provider.users[index].imageUrl))),
+                    ),
+                  ),
+                ),
+                sizedBoxH(8),
+                BounceFromBottomAnimation(
+                  delay: 3,
+                  child: Text(
+                    provider.users[index].name,
+                    style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 22),
+                  ),
+                ),
+                sizedBoxH(4),
+                BounceFromBottomAnimation(
+                  delay: 3,
+                  child: Text(
+                    provider.users[index].pleace,
+                    style: TextStyle(fontSize: 18, color: Colors.grey.shade400),
+                  ),
+                ),
+                BounceFromBottomAnimation(
+                  delay: 3,
+                  child: Container(
+                    margin: const EdgeInsets.symmetric(vertical: 24),
+                    decoration: BoxDecoration(borderRadius: BorderRadius.circular(12), color: const Color(0xFFFDEAE8)),
+                    child: Padding(
+                      padding: const EdgeInsets.all(6.0),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          const Icon(
+                            Icons.favorite,
+                            color: Colors.red,
                           ),
+                          sizedBoxW(4),
+                          Text(provider.users[index].favorites.toString())
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                const Divider(),
+                sizedBoxH(12),
+                BounceFromBottomAnimation(
+                  delay: 3,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Bio",
+                        style: TextStyle(color: Colors.grey.shade500),
+                      ),
+                      sizedBoxH(8),
+                      const Text(
+                        "Cat-lover, Designer, Weekend fan and biologist",
+                        style: TextStyle(fontSize: 18, color: Color(0xFF5E5E5E)),
+                      ),
+                      sizedBoxH(24),
+                      BounceFromBottomAnimation(
+                        delay: 3,
+                        child: Row(
+                          children: [
+                            Expanded(
+                              child: ElevatedButton(
+                                  onPressed: () {},
+                                  style: ElevatedButton.styleFrom(
+                                      backgroundColor: AppColors.orangeColor,
+                                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12))),
+                                  child: const Center(
+                                      child: Padding(
+                                    padding: EdgeInsets.symmetric(vertical: 16.0),
+                                    child: Text(
+                                      "Send Message",
+                                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                                    ),
+                                  ))),
+                            ),
+                            sizedBoxW(12),
+                            GestureDetector(
+                              onTapDown: (detail) {
+                                selectOperationMenu(context, detail);
+                              },
+                              child: Container(
+                                decoration:
+                                    BoxDecoration(shape: BoxShape.circle, border: Border.all(color: Colors.grey)),
+                                child: const Padding(
+                                  padding: EdgeInsets.all(14.0),
+                                  child: Icon(Icons.more_horiz_outlined),
+                                ),
+                              ),
+                            )
+                          ],
                         ),
                       )
                     ],
-                  )
-                ],
-              ),
-            ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       );
